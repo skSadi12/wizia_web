ef52a4c8c3f0a2a2d9164b6ca4c54514651da49b
skSadi12 <sksadi433@gmail.com>
2025-04-28 00:23:50 +0600

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 function Hero() {
   return (
-    <section className='bg-[url(Imgf.png)] text-white' >
-      
+    <section className="bg-[url(Imgf.png)]  text-white w-full bg-bottom-right md:bg-cover">
+      <div className="max-w-[1400px] mx-auto">
+        <div className="max-w-[500px] space-y-12 py-16 px-5" >
+          <h3 className="text-[#0FF1F6] text-[24px]">AI SDRs (aiDRs)</h3>
+          <h2 className="text-[#FFFFFF] text-[64px]">More leads, less people.</h2>
+          <p className="text-gray-300 text-[20px]">
+            Train an aiDR on your ICP and messaging matrix. Activate it on a
+            patch. It will send personalized sequences to every target contact.
+          </p>
+          <button className="bg-[#0FF1F6] text-xl px-5 py-2 text-black rounded-[45px] hover:bg-white hover:text-black">
+            Sign Up for the Beta 
+            <OpenInNewIcon/>
+          </button>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
