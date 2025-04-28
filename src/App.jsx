2acaac10837d9eca90b5ee601589981c1fd19ca9
skSadi12 <sksadi433@gmail.com>
2025-04-29 00:09:19 +0600

    doing aidr component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbarx from './components/Navbar'
 import Hero from './components/Hero'
 import Partners from './components/partners'
+import Aidr from './components/Aidr'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Navbarx/>
       <Hero/>
       <Partners/>
+      <Aidr/>
     </>
   )
 }
---
+++ src/components/Aidr.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import pic1 from '/ph1.png'
+import pic2 from '/ph2.png'
+import pic3 from '/ph3.png'
+
+function Aidr() {
+  return (
+    <section className='bg-[#002228]'>
+      <div className='max-w-[1400px] mx-auto flex py-10 px-5 flex-col md:flex-row gap-6'>
+        <div className='space-y-3  w-full md:w-1/3'>
+            <img src={pic1} alt="" />
+            <h5 className='text-[28px] font-bold text-white'>You’re in Control</h5>
+            <p className='text-[20px] text-gray-400'>aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work.</p>
+        </div>
+        <div className='space-y-3 w-full md:w-1/3'>
+            <img src={pic2} alt="" />
+            <h5 className='text-[28px] font-bold text-white'>Infinitely Scalable</h5>
+            <p className='text-[20px] text-gray-400'>Train an aiDR on a patch. When you are happy with the results, scale it effortlessly.</p>
+        </div>
+        <div className='space-y-3 w-full md:w-1/3'>
+            <img src={pic3} alt="" />
+            <h5 className='text-[28px] font-bold text-white'>Incredibly Flexible</h5>
+            <p className='text-[20px] text-gray-400'>Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Aidr
